4d9286dd646374eaf2bbd9faf9a9d4b9806f5a49
Ahmeddzz <ahmedzahran95@hotmail.com>
2023-03-29 13:38:37 +0200

    First project commit

---
+++ src/main/java/com/ahmedzahran/cafemangementbackend/CafeMangementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ahmedzahran.cafemangementbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeMangementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeMangementBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ahmedzahran/cafemangementbackend/CafeMangementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmedzahran.cafemangementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeMangementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
