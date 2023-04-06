f3555da9b1f865c8e46c499edd0c7460c2ceaa7c
Ahmeddzz <ahmedzahran95@hotmail.com>
2023-04-06 12:38:57 +0200

    Add product model

---
+++ src/main/java/com/ahmedzahran/cafemangementbackend/model/Product.java
@@ -0,0 +1,39 @@
+package com.ahmedzahran.cafemangementbackend.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.io.Serializable;
+
+@Data
+@Entity
+@DynamicInsert
+@DynamicUpdate
+@Table(name="product")
+public class Product implements Serializable {
+
+
+    public static final long serialVersionUid = 123456L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+    @Column(name="name")
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="category_fk",nullable = false)
+    private Category category;
+
+    @Column(name="description")
+    private String description;
+
+    @Column(name="price")
+    private Integer price;
+
+    @Column(name="status")
+    private String status;
+}
